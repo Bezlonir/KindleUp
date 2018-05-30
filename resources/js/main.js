d387299c448e632b2a31184497792205797d47c3
Bezlonir <michael_bragg99@hotmail.com>
2018-05-30 12:47:58 -0600

    Create initial file structure and API request script.

---
+++ resources/js/main.js
@@ -0,0 +1,31 @@
+var KindleUp = {};
+var API_KEY = 'pZllHOn0wv6CcJj03AgKEQ';
+var secret = 'MwyWN1NSqEdFsYa2ueR2ZMEmZ5zmzI5Oijdamf0TiM';
+var url = "https://www.goodreads.com/search/index.xml?";
+
+
+// runtime
+$(document).ready(function() {
+  $("#search-button").click(function(){
+    var searchText = $("#search-input").val();
+    if (searchText) {
+      KindleUp.searchBooks(searchText);
+    }
+  });
+  //simulate a search button keypress when enter is pressed on the keyboard in the input box
+  $("#search-input").keypress(function(e){
+    if(e.which === 13){
+      $("#search-button").click();
+    }
+  });
+});
+
+KindleUp.searchBooks = function(searchText) {
+  $.ajax({
+    url:url + API_KEY + '&q=' + searchText + '&format=json',
+    datatype:'jsonp',
+    success: function(result) {
+      console.log(result);
+    }
+  });
+}
